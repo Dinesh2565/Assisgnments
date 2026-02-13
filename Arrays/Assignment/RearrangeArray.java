e077ab83a97fb27807d9c2574fb8eed295fa6a40
Dinesh2565 <dineshsachin2565@gmail.com>
2026-02-13 15:49:28 +0530

    Rearrange array

---
+++ Arrays/Assignment/RearrangeArray.java
@@ -0,0 +1,22 @@
+package Arrays.Assignment;
+
+public class RearrangeArray {
+    public int[] rearrangeArray(int[] nums) {
+        int [] ans= new int [nums.length];
+        int p=0;
+        int n=1;
+        int i=0;
+        while (i<nums.length){
+            if(nums[i]<0){
+                ans[n]=nums[i];
+                n+=2;
+            }
+            else{
+                ans[p]=nums[i];
+                p+=2;
+            }
+            i++;
+        }
+        return ans;
+    }
+}
